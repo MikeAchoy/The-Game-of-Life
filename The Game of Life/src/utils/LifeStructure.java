112a0aeb4c640f19bc0a08f4cf8946f84fbcaf61
Mikeab <97768019+MikeAchoy@users.noreply.github.com>
2025-02-04 22:10:22 -0600

    Added LifeStructure as separate importable enum for use in multiple files.

---
+++ The Game of Life/src/utils/LifeStructure.java
@@ -0,0 +1,22 @@
+package utils;
+
+public enum LifeStructure {
+    SINGLE_PIXEL, // Default Setting
+
+    BLOCK,        // Still Lives
+    BEEHIVE,
+    LOAF,
+    BOAT,
+    TUB,
+
+    BLINKER,      // Oscillators
+    TOAD,
+    BEACON,
+    PULSAR,
+    PENTADECATHLON,
+
+    GLIDER,       // Spaceships
+    SMALL_SPACESHIP,
+    MEDIUM_SPACESHIP,
+    LARGE_SPACESHIP
+}
